97d5be6c232aa0da6acc8267b4c7d2626a3275fd
greeshmarllp <48478457+greeshmarllp@users.noreply.github.com>
2020-11-12 15:42:27 +0530

    save

--- Themes/Storefront/resources/assets/public/js/components/home/TopBrands.vue
+++ Themes/Storefront/resources/assets/public/js/components/home/TopBrands.vue
@@ -1,18 +1,108 @@
 <template>
-    <section class="top-brands-wrap clearfix">
-        <div class="container">
-            <div class="top-brands clearfix">
-                <a
-                    v-for="(topBrand, index) in topBrands"
-                    :key="index"
-                    :href="topBrand.url"
-                    class="top-brand-image"
-                >
-                    <img :src="topBrand.logo.path" alt="brand logo">
-                </a>
+
+     <div class="banner_area banner_gallery2">
+            <div class="container">
+                <div class="container">
+                    <div class="col-12">
+                        <div class="section_title">
+                            <h2  class="head-stripe">SHOP BY BRANDS</h2>
+                        </div>
+                    </div>
+                </div>
+               <div class="row">
+            <div class="col-md-2 col-6" id="p1">
+                <div class="single_product">
+                    <div class="product_thumb">
+                       
+                                        <a class="primary_img" href="product-details.html"><img src="assets/img/brand/Codival.jpg" alt=""></a>
+                                        <a class="secondary_img" href="product-details.html"><img src="assets/img/brand/Codival.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 col-6 col-half-offset" id="p2">
+                <div class="single_product">
+                    <div class="product_thumb">
+                        <a class="primary_img" href="product-details.html"><img src="assets/img/brand/Etiq-Etal.jpg" alt=""></a>
+                        <a class="secondary_img" href="product-details.html"><img src="assets/img/brand/Etiq-Etal.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 col-6 col-half-offset" id="p3">
+                <div class="single_product">
+                    <div class="product_thumb">
+                        <a class="primary_img" href="product-details.html"><img src="assets/img/brand/icel.jpg" alt=""></a>
+                        <a class="secondary_img" href="product-details.html"><img src="assets/img/brand/icel.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 col-6 col-half-offset" id="p4">
+                <div class="single_product">
+                    <div class="product_thumb">
+                        <a class="primary_img" href="product-details.html"><img src="assets/img/brand/Martellato.jpg" alt=""></a>
+                        <a class="secondary_img" href="product-details.html"><img src="assets/img/brand/Martellato.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 col-6 col-half-offset" id="p5">
+                <div class="single_product">
+                    <div class="product_thumb">
+                        <a class="primary_img" href="product-details.html"><img src="assets/img/brand/PME.jpg" alt=""></a>
+                         <a class="secondary_img" href="product-details.html"><img src="assets/img/brand/PME.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 col-6" id="p1">
+                <div class="single_product">
+                    <div class="product_thumb">
+                        <a class="primary_img" href="product-details.html"><img src="assets/img/brand/Polenfood.jpg" alt=""></a>
+                        <a class="secondary_img" href="product-details.html"><img src="assets/img/brand/Polenfood.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 col-6 col-half-offset" id="p2">
+                <div class="single_product">
+                    <div class="product_thumb">
+                        <a class="primary_img" href="product-details.html"><img src="assets/img/brand/santos.jpg" alt=""></a>
+                        <a class="secondary_img" href="product-details.html"><img src="assets/img/brand/santos.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 col-6 col-half-offset" id="p3">
+                <div class="single_product">
+                    <div class="product_thumb">
+                        <a class="primary_img" href="product-details.html"><img src="assets/img/brand/SASA-Demarle.jpg" alt=""></a>
+                         <a class="secondary_img" href="product-details.html"><img src="assets/img/brand/SASA-Demarle.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 col-6 col-half-offset" id="p4">
+                <div class="single_product">
+                    <div class="product_thumb">
+                        <a class="primary_img" href="product-details.html"><img src="assets/img/brand/Sugarflair%20Colours.jpg" alt=""></a>
+                        <a class="secondary_img" href="product-details.html"><img src="assets/img/brand/Sugarflair%20Colours.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 col-6 col-half-offset" id="p5">
+                <div class="single_product">
+                    <div class="product_thumb">
+                        <a class="primary_img" href="product-details.html"><img src="assets/img/brand/Sugarflair.jpg" alt=""></a>
+                        <a class="secondary_img" href="product-details.html"><img src="assets/img/brand/Sugarflair.jpg" alt=""></a>
+                    </div>
+                </div>
             </div>
         </div>
-    </section>
+                <div class="row">
+                    <div class="col-12" style="margin-top: 35px">
+                        <a href="" style="padding: 15px; font-size: 18px; border-radius: 20px;font-weight: 500;float: right">See More</a>
+                    </div>
+                    </div>
+            </div>
+        </div>
+
+
+
+
 </template>
 
 <script>
