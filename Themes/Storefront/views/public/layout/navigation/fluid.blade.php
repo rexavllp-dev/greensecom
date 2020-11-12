b526d52890bdeb6455e5153eb22d6922a78ce8fc
greeshmarllp <48478457+greeshmarllp@users.noreply.github.com>
2020-11-12 13:06:45 +0530

    commit

--- Themes/Storefront/views/public/layout/navigation/fluid.blade.php
+++ Themes/Storefront/views/public/layout/navigation/fluid.blade.php
@@ -1,21 +1,32 @@
-
-
 <ul class="sub_menu" style="width: 1140px;background-image: url({{ $menu->backgroundImage() }});background-position: center;">
     <div class="container">
         <div class="row">
-                        @foreach ($subMenus as $subMenu)
+
+            @php
+            $count = 1;
+            @endphp
             <div class="col-md-3">
-                <h4  style="margin-top: 10px;"><a href="{{ $subMenu->url() }}" target="{{ $subMenu->target() }}">
+            @foreach ($subMenus as $subMenu)
+            <h4  style="margin-top: 10px;"><a href="{{ $subMenu->url() }}" target="{{ $subMenu->target() }}">
                             {{ $subMenu->name() }}
                         </a></h4>
-                @foreach ($subMenu->items() as $item)
-                <li><a href="{{ $item->url() }}" target="{{ $subMenu->target() }}">
-                                    {{ $item->name() }}
-                                </a></li>
-                @endforeach
-
+            @foreach ($subMenu->items() as $item)
+            <li><a href="{{ $item->url() }}" target="{{ $subMenu->target() }}">
+                {{ $item->name() }}
+            </a></li>
+            @if($count % 15 == 0)
             </div>
-        @endforeach
+            <div class="col-md-3">
+            @endif
+            @php
+            $count++;
+            @endphp
+            @endforeach
+            @php
+            $count++;
+            @endphp
+            @endforeach
+            </div>
         </div>
     </div>
 </ul>
