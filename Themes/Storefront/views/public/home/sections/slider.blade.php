b4d229ea6ab7d067eb9afad4c0f49446205bbd66
amalkshaji <amal.shaji@rexav.in>
2020-11-12 13:01:18 +0530

    save

--- Themes/Storefront/views/public/home/sections/slider.blade.php
+++ Themes/Storefront/views/public/home/sections/slider.blade.php
@@ -1,59 +1,32 @@
-<section class="home-section-wrap">
-    <div class="container">
-        <div class="row">
-            <div class="home-section-inner">
-                <div class="home-slider-wrap">
-                    <div
-                        class="home-slider"
-                        data-speed="{{ $slider->speed ?? '1000' }}"
-                        data-autoplay="{{ $slider->autoplay ?? 'false' }}"
-                        data-autoplay-speed="{{ $slider->autoplay_speed ?? '5000' }}"
-                        data-fade="{{ $slider->fade ?? 'false' }}"
-                        data-dots="{{ $slider->dots ?? 'true' }}"
-                        data-arrows="{{ $slider->arrows ?? 'true' }}"
-                    >
-                        @foreach ($slider->slides as $slide)
-                            <div class="slide">
-                                <img src="{{ $slide->file->path }}" data-animation-in="zoomInImage" class="slider-image animated">
+<section class="slider_section mb-70">
+            <div class="slider_area owl-carousel">
+                @foreach ($slider->slides as $slide)
+                 <div class="single_slider d-flex align-items-center" data-bgimg="{{ $slide->file->path }}">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="slider_content">
+                                    <h1>{!! $slide->caption_1 !!}</h1>
+                                    <h2>{!! $slide->caption_2 !!}</h2>
 
-                                <div class="slide-content {{ $slide->isAlignedLeft() ? 'align-left' : 'align-right' }}">
-                                    <div class="captions">
-                                        <span
-                                            class="caption caption-1"
-                                            data-animation-in="{{ data_get($slide->options, 'caption_1.effect', 'fadeInRight') }}"
-                                            data-delay-in="{{ data_get($slide->options, 'caption_1.delay', '0') }}"
-                                        >
-                                            {!! $slide->caption_1 !!}
-                                        </span>
-
-                                        <span
-                                            class="caption caption-2"
-                                            data-animation-in="{{ data_get($slide->options, 'caption_2.effect', 'fadeInRight') }}"
-                                            data-delay-in="{{ data_get($slide->options, 'caption_2.delay', '0.3') }}"
-                                        >
-                                            {!! $slide->caption_2 !!}
-                                        </span>
-
-                                        @if ($slide->call_to_action_text)
-                                            <a
+                                    @if ($slide->call_to_action_text)
+                                    <br>
+                                    <a
                                                 href="{{ $slide->call_to_action_url }}"
                                                 class="btn btn-primary btn-slider"
-                                                data-animation-in="{{ data_get($slide->options, 'call_to_action.effect', 'fadeInRight') }}"
-                                                data-delay-in="{{ data_get($slide->options, 'call_to_action.delay', '0.7') }}"
                                                 target="{{ $slide->open_in_new_window ? '_blank' : '_self' }}"
                                             >
                                                 {!! $slide->call_to_action_text !!}
                                             </a>
-                                        @endif
-                                    </div>
+                                    @endif
                                 </div>
                             </div>
-                        @endforeach
+                        </div>
                     </div>
                 </div>
-
-                @include('public.home.sections.slider_banners')
+                @endforeach
             </div>
-        </div>
-    </div>
 </section>
+
+
+
--- Themes/Storefront/views/public/layout/navigation/fluid.blade.php
+++ Themes/Storefront/views/public/layout/navigation/fluid.blade.php
@@ -1,35 +1,20 @@
-
-
 <ul class="sub_menu" style="width: 1140px;background-image: url({{ $menu->backgroundImage() }});background-position: center;">
     <div class="container">
         <div class="row">
-                        @foreach ($subMenus as $subMenu)
+            @foreach ($subMenus as $subMenu)
             <div class="col-md-3">
-                <h4  style="margin-top: 10px;"><a href="{{ $subMenu->url() }}" target="{{ $subMenu->target() }}">
-                            {{ $subMenu->name() }}
-                        </a></h4>
+                <h4  style="margin-top: 10px;">
+                <a href="{{ $subMenu->url() }}" target="{{ $subMenu->target() }}">
+                    {{ $subMenu->name() }}
+                </a></h4>
                 @foreach ($subMenu->items() as $item)
-                <li><a href="{{ $item->url() }}" target="{{ $subMenu->target() }}">
-                                    {{ $item->name() }}
-                                </a></li>
+                <li>
+                <a href="{{ $item->url() }}" target="{{ $subMenu->target() }}">
+                    {{ $item->name() }}
+                </a></li>
                 @endforeach
-
             </div>
-        @endforeach
+            @endforeach
         </div>
     </div>
 </ul>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
